f3535c7d834e277faa74f6600f196264a9630ff5
Stanislav <stasgrisha2@yandex.ru>
2023-03-05 22:07:07 +0300

    All tasks changed

---
+++ src/HM1/Task1.java
@@ -0,0 +1,23 @@
+package HM1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner input =  new Scanner (System.in);
+        System.out.print("enter number: ");
+        int number = input.nextInt();
+        input.close();
+
+
+
+        int sum = 0, multiply = 1;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+            multiply *= i;
+        }
+        System.out.printf("n-ое треугольного число = %d \n", sum);
+        System.out.printf("произведение чисел от 1 до %d : %d", number, multiply);
+    }
+}
---
+++ src/HM1/Task2.java
@@ -0,0 +1,23 @@
+package HM1;
+
+public class Task2 {
+    //    Вывести все простые числа от 1 до 1000
+
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000; i++) {
+            if (simpleNumber(i)){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+    public static boolean simpleNumber(int num){
+        for (int i = 2; i <= num/2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
